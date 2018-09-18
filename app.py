4885b1b7ce7f8e29233c2ab88a1867ba9cd6550f
Mario <krajacic.mario@gmail.com>
2018-09-18 06:04:50 +0200

    Adding home and about page

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# rute do stranica
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return '<h1>Hello World!!!</h1>'
 
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
