2d19eae188dd5553bf8388c2a58d5b72a2fba5cd
Mario Krajacic <krajacic.mario@gmail.com>
2018-09-18 15:54:02 +0200

    Dodavanje strukture predložaka

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Hello World!!!</h1>'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About page</h1>'
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
